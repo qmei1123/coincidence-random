bcc20dfdb3c2001d63b7179eeca0a547f289d5a6
qmei1123 <qmei@bu.edu>
2017-10-06 00:18:49 -0400

    Initial commit
    Create files, add rudimentary html. Explore particle.js functions

---
+++ js/app.js
@@ -0,0 +1,149 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 100,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff",
+        "value": "#2f1c1c",
+        "value": "#df247c",
+        "value": "#df247c",
+        "value": "#f1fefd",
+        "value": "#26c580",
+        "value": "#ff8866",
+        "value": "#ff00ca",
+        "value": "#d8fcf3",
+        "value": "#11f2ce",
+        "value": "#ffffff",
+        "value": "#ffffff",
+        "value": "#ffffff",
+        "value": "#ffffff",
+        "value": "#ffffff",
+        "value": "random"
+      },
+      "shape": {
+          "random": false,
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 3
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": true,
+          "speed": 1,
+          "opacity_min": 1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 50,
+          "size_min": 0.2,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 50,
+        "color": "#ffffff",
+        "opacity": 1,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": true,
+        "attract": {
+          "enable": true,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push",
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 100,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
