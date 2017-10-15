ec2c3d503d57e8cd6ad99536aeab9c5b547e8376
qmei1123 <qmei@bu.edu>
2017-10-15 15:33:45 -0400

    Attempt to edit JS

--- js/js.js
+++ js/js.js
@@ -1,5 +1,8 @@
-$(document).ready();
-
-$("#particle-js").click(function(){
-    $(".text").hide();
+$(document).ready(function(){
+    $(".text-div").on("click", function(){
+        $(".text").toggle();
+    });
 });
+
+$(".text").css("color", "#000");
+$(canvas-el).hide();
